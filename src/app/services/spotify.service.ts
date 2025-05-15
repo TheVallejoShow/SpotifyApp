debceb38ecd5a5ea4e32cc969110e79c4078ae3e
Diego Vallejo <dieho60714@gmail.com>
2025-05-14 19:12:56 -0500

    Add functions to get new releases, search artists, fetch artist details and top tracks

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {  }
+
+  getQuery( query: string ) {
+    const url = `https://api.spotify.com/v1/${ query }`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQDsnU8IhPdDKrUXuWq7FVMlrNmZrmMJiSyG6fRESINYgAJBBpnLiQ6GThLMuQsxqlLJVavErt4qQAgzvrA'
+    });
+
+    return this.http.get(url, { headers });
+
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?limit=20')
+      .pipe( map((data: any) => data['albums'].items));
+  }
+
+  getArtists( termino: string ) {
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+      .pipe( map((data: any) => data['artists'].items));
+  }
+
+  getArtist( id: string ) {
+    return this.getQuery(`artists/${ id }`);
+  }
+
+  getTopTracks( id: string ) {
+    return this.getQuery(`artists/${ id }/top-tracks?country=us`)
+      .pipe( map((data: any) => data['tracks'].items));
+  }
 }
